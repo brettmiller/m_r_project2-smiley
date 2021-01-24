2510528e023fbe0746f718cfe5d0ec2cd78f8831
Brett Miller <brett.miller@vitalsource.com>
2021-01-24 13:54:26 -0600

    servo testing

--- src/m_r_project2-smiley.cpp
+++ src/m_r_project2-smiley.cpp
@@ -2,27 +2,40 @@
  * PIR sensor tester
  */
 #include <Arduino.h>
-#include <Server.h>
+#include <Servo.h>
 
-int ledPin = 2;               // choose the pin for the LED
-int inputPin = 4;               // choose the input pin (for PIR sensor)
-int pirState = LOW;           // we start, assuming no motion detected
-int val = 0;                    // variable for reading the pin status
+Servo servo;
+
+int ledPin = 2;                 // choose the pin for the LED
+int inputPin = 4;               // D2 - choose the input pin (for PIR sensor)
+int servoPin = 5;               // D5
+int pirState = LOW;             // we start, assuming no motion detected
+int val      = 0;               // variable for reading the pin status
  
 void setup() {
   pinMode(ledPin, OUTPUT);      // declare LED as output
   pinMode(inputPin, INPUT);     // declare sensor as input
- 
+  servo.attach(servoPin); 
+  servo.write(0);
+  delay(1000);
+
   Serial.begin(9600);
 }
  
 void loop(){
   val = digitalRead(inputPin);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
-    digitalWrite(ledPin, LOW);  // turn LED ON
+        digitalWrite(ledPin, LOW);  // turn LED ON
     if (pirState == LOW) {
       // we have just turned on
-      Serial.println("Motion detected!");
+    /*  Serial.println("Motion detected!");
+      servo.write(0);
+      delay(1000);
+      servo.write(90);
+      delay(1000);
+      servo.write(45);
+      delay(1000); */
+
       // We only want to print on the output change, not state
       pirState = HIGH;
     }
