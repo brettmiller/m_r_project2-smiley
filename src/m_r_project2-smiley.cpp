515619d0983aab4e33c8aa8a3c74762d3138374a
Brett Miller <brett.miller@vitalsource.com>
2021-01-24 16:28:25 -0600

    more testing

--- src/m_r_project2-smiley.cpp
+++ src/m_r_project2-smiley.cpp
@@ -6,45 +6,50 @@
 
 Servo servo;
 
-int ledPin = 2;                 // choose the pin for the LED
-int inputPin = 4;               // D2 - choose the input pin (for PIR sensor)
-int servoPin = 5;               // D5
+//int ledPin = 2;                 // choose the pin for the LED
+int pirPin = 14;               // D5 (GPIO14) or D2 (GPIO4) - choose the input pin (for PIR sensor)
+int servoPin = 5;               // D1 GPIO5
 int pirState = LOW;             // we start, assuming no motion detected
-int val      = 0;               // variable for reading the pin status
+int pirval   = 0;               // variable for reading the pin status
  
 void setup() {
-  pinMode(ledPin, OUTPUT);      // declare LED as output
-  pinMode(inputPin, INPUT);     // declare sensor as input
+  //pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(pirPin, INPUT);     // declare sensor as input
   servo.attach(servoPin); 
   servo.write(90);
   delay(1000);
+  servo.detach();
 
   Serial.begin(9600);
 }
  
 void loop(){
-  val = digitalRead(inputPin);  // read input value
-  if (val == HIGH) {            // check if the input is HIGH
-        digitalWrite(ledPin, LOW);  // turn LED ON
-    if (pirState == LOW) {
-      // we have just turned on
-      Serial.println("Motion detected!");
-      servo.write(0);
-      delay(1000);
-      servo.write(180);
-      delay(1000);
-      servo.write(90);
-      delay(1000);
+  pirval = digitalRead(pirPin);  // read input value
+  if (pirval == HIGH) {            // check if the input is HIGH
+   // digitalWrite(ledPin, LOW);  // turn LED ON
+   if (pirState == LOW) {
+     // just turned on
+     Serial.println("Motion detected!");
+     servo.attach(servoPin); 
+     while (pirval == HIGH) {
+       servo.write(0);
+       delay(1000);
+       servo.write(180);
+       delay(1200);
+       pirval = digitalRead(pirPin);
+     }            
 
-      // We only want to print on the output change, not state
-      pirState = HIGH;
-    }
+     pirState = HIGH;
+   }
   } else {
-    digitalWrite(ledPin, HIGH); // turn LED OFF
-    if (pirState == HIGH){
-      // we have just turned of
+   // digitalWrite(ledPin, HIGH); // turn LED OFF
+    if (pirState == HIGH) {
+      // we have just turned off
       Serial.println("Motion ended!");
       // We only want to print on the output change, not state
+      servo.write(90);
+      delay(1000);
+      servo.detach();
       pirState = LOW;
     }
   }
